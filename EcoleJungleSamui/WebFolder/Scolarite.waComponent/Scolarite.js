c1238beb2807607b1105594b1e103d4c7d03e850
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-08-31 19:27:51 +0200

    Update #39

--- EcoleJungleSamui/WebFolder/Scolarite.waComponent/Scolarite.js
+++ EcoleJungleSamui/WebFolder/Scolarite.waComponent/Scolarite.js
@@ -13,19 +13,185 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var cFin = {};	// @textField
+	var cDeb = {};	// @textField
+	var cFullA = {};	// @checkbox
+	var btUndo = {};	// @button
+	var btIns = {};	// @buttonImage
+	var cbEleve = {};	// @combobox
 	var cbAnScol = {};	// @combobox
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	cFin.change = function cFin_change (event)// @startlock
+	{// @endlock
+		var vConvA, vConvB, vConvD, vConvF, vAnDeb, vAnFin, vDeb, vFin;
+		
+		vConvD = $$("component1_cAnDeb").getValue();
+		vAnDeb = new Date(vConvD.substr(6,4), parseInt(vConvD.substr(3,2))-1, vConvD.substr(0,2));
+		vConvF = $$("component1_cAnFin").getValue();
+		vAnFin = new Date(vConvF.substr(6,4), parseInt(vConvF.substr(3,2))-1, vConvF.substr(0,2));
+		vConvA = $$("component1_cDeb").getValue();
+		vDeb = new Date(vConvA.substr(6,4), parseInt(vConvA.substr(3,2))-1, vConvA.substr(0,2));
+		vConvB = $$("component1_cFin").getValue();
+		vFin = new Date(vConvB.substr(6,4), parseInt(vConvB.substr(3,2))-1, vConvB.substr(0,2));
+		if ((vConvB !== null) && (vFin < vAnDeb || vFin > vAnFin)) {
+			alert("Cette date doit être comprise dans la période de l'année scolaire [" + vConvD + " - " + vConvF + "]");
+			$$("component1_cFin").setValue("");
+		} else {
+			if ((vConvA !== null) && ( vDeb > vFin)) {
+				alert("La date 'Commence le : ' doit être inférieure à la date 'Termine le : '");
+				$$("component1_cFin").setValue("");
+			}
+		}
+	};// @lock
+
+	cDeb.change = function cDeb_change (event)// @startlock
+	{// @endlock
+		var vConvA, vConvB, vConvD, vConvF, vAnDeb, vAnFin, vDeb, vFin;
+		
+		vConvD = $$("component1_cAnDeb").getValue();
+		vAnDeb = new Date(vConvD.substr(6,4), parseInt(vConvD.substr(3,2))-1, vConvD.substr(0,2));
+		vConvF = $$("component1_cAnFin").getValue();
+		vAnFin = new Date(vConvF.substr(6,4), parseInt(vConvF.substr(3,2))-1, vConvF.substr(0,2));
+		vConvA = $$("component1_cDeb").getValue();
+		vDeb = new Date(vConvA.substr(6,4), parseInt(vConvA.substr(3,2))-1, vConvA.substr(0,2));
+		vConvB = $$("component1_cFin").getValue();
+		vFin = new Date(vConvB.substr(6,4), parseInt(vConvB.substr(3,2))-1, vConvB.substr(0,2));
+		if ((vConvA !== null) && (vDeb < vAnDeb || vDeb > vAnFin)) {
+			alert("Cette date doit être comprise dans la période de l'année scolaire [" + vConvD + " - " + vConvF + "]");
+			$$("component1_cDeb").setValue("");
+		} else {
+			if ((vConvB !== null) && ( vDeb > vFin)) {
+				alert("La date 'Commence le : ' doit être inférieure à la date 'Termine le : '");
+				$$("component1_cDeb").setValue("");
+			}
+		}
+		
+	};// @lock
+
+	cFullA.click = function cFullA_click (event)// @startlock
+	{// @endlock
+		var vFullA = $$("component1_cFullA").getValue();
+		//alert(vFullA);
+		
+		if (vFullA) {
+			$$("component1_cDeb").setReadOnly(true);
+			$$("component1_cFin").setReadOnly(true);
+			$$("component1_cDeb").setValue($$("component1_cAnDeb").getValue());
+			$$("component1_cFin").setValue($$("component1_cAnFin").getValue());
+		} else {
+			$$("component1_cDeb").setReadOnly(false);
+			$$("component1_cFin").setReadOnly(false);
+		}
+	};// @lock
+
+	btUndo.click = function btUndo_click (event)// @startlock
+	{// @endlock
+		
+		var vAnScol, now, vAnDeb, vAnFin, vConv;
+		
+		$$("component1_cFiche").show();
+		$$("component1_btSave").hide();
+		$$("component1_btUndo").hide();
+		$$("component1_btIns").show();
+		$$("component1_cbEleve").hide();
+		$$("component1_cbModR").hide();
+		$$("component1_cModeR").show();
+		$$("component1_cFullA").disable();
+		
+		$$("component1_cDeb").setReadOnly(true);
+		$$("component1_cFin").setReadOnly(true);
+		
+		
+		
+		vAnScol = $$("component1_cbAnScol").getValue();
+		$$("component1_cFullA").disable();
+		sources.component1_parcours_Scolaire.query("Annee_Scolaire.ID = :1 order by Eleve.Nom_complet", { onSuccess: function(event) { 
+		
+			now = new Date();
+			sources.component1_eleves.query("Utilisateur.Date_Entree < :1 AND (Utilisateur.Date_Sortie = null OR Utilisateur.Date_Sortie > :1)", now, vAnScol);
+			vConv = $$("component1_cAnDeb").getValue();
+			vAnDeb = new Date(vConv.substr(6,4), parseInt(vConv.substr(3,2))-1, vConv.substr(0,2));
+			vConv = $$("component1_cAnFin").getValue();
+			vAnFin = new Date(vConv.substr(6,4), parseInt(vConv.substr(3,2))-1, vConv.substr(0,2));
+			if ((now > vAnDeb) && (now < vAnFin)) {
+				$$("component1_btIns").show();
+			} else {
+				$$("component1_btIns").hide();
+			}
+			
+			}, params:[vAnScol] }); 		
+	};// @lock
+
+	btIns.click = function btIns_click (event)// @startlock
+	{// @endlock
+		//var CFonction;
+		sources.component1_parcours_Scolaire.addNewElement();
+		
+		$$("component1_cFiche").hide();
+		$$("component1_btSave").show();
+		$$("component1_btUndo").show();
+		$$("component1_btIns").hide();
+		$$("component1_cbEleve").show();
+		$$("component1_cbModR").show();
+		$$("component1_cModeR").hide();
+		$$("component1_cFullA").enable();
+		
+		$$("component1_cFullA").check();
+		$$("component1_cDeb").setValue($$("component1_cAnDeb").getValue());
+		$$("component1_cFin").setValue($$("component1_cAnFin").getValue());
+		
+	};// @lock
+
+	cbEleve.change = function cbEleve_change (event)// @startlock
+	{// @endlock
+		var vAnScol, vEleve, vNb;
+		vAnScol = $$("component1_cbAnScol").getValue();
+		vEleve = $$("component1_cbEleve").getValue();
+		sources.psCtrl.query("Annee_Scolaire.ID = :1 and Eleve.ID = :2",{ onSuccess: function(event) { 
+			vNb = sources.psCtrl.length;
+			if (vNb > 0) {
+				$$("component1_btSave").disable();
+			} else {
+				$$("component1_btSave").enable();
+			}
+			}, params:[vAnScol, vEleve] }); 
+		
+		
+	};// @lock
+
 	cbAnScol.change = function cbAnScol_change (event)// @startlock
 	{// @endlock
-		var vAnScol = $$("component1_cbAnScol").getValue();
-		sources.component1_parcours_Scolaire.query("Annee_Scolaire.ID = :1 order by Eleve.Nom_complet", vAnScol);
+		var vAnScol, now, vAnDeb, vAnFin, vConv;
+		vAnScol = $$("component1_cbAnScol").getValue();
+		$$("component1_cFullA").disable();
+		sources.component1_parcours_Scolaire.query("Annee_Scolaire.ID = :1 order by Eleve.Nom_complet", { onSuccess: function(event) { 
+		
+			now = new Date();
+			sources.component1_eleves.query("Utilisateur.Date_Entree < :1 AND (Utilisateur.Date_Sortie = null OR Utilisateur.Date_Sortie > :1)", now, vAnScol);
+			vConv = $$("component1_cAnDeb").getValue();
+			vAnDeb = new Date(vConv.substr(6,4), parseInt(vConv.substr(3,2))-1, vConv.substr(0,2));
+			vConv = $$("component1_cAnFin").getValue();
+			vAnFin = new Date(vConv.substr(6,4), parseInt(vConv.substr(3,2))-1, vConv.substr(0,2));
+			if ((now > vAnDeb) && (now < vAnFin)) {
+				$$("component1_btIns").show();
+			} else {
+				$$("component1_btIns").hide();
+			}
+			
+			}, params:[vAnScol] }); 		
 		
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_cFin", "change", cFin.change, "WAF");
+	WAF.addListener(this.id + "_cDeb", "change", cDeb.change, "WAF");
+	WAF.addListener(this.id + "_cFullA", "click", cFullA.click, "WAF");
+	WAF.addListener(this.id + "_btUndo", "click", btUndo.click, "WAF");
+	WAF.addListener(this.id + "_btIns", "click", btIns.click, "WAF");
+	WAF.addListener(this.id + "_cbEleve", "change", cbEleve.change, "WAF");
 	WAF.addListener(this.id + "_cbAnScol", "change", cbAnScol.change, "WAF");
 	// @endregion// @endlock
 
