622212e79d45c38355d79a8c1d00823285563ee0
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-08-31 03:54:42 +0200

    Update #38

--- EcoleJungleSamui/WebFolder/Scolarite.waComponent/Scolarite.js
+++ EcoleJungleSamui/WebFolder/Scolarite.waComponent/Scolarite.js
@@ -13,11 +13,20 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var cbAnScol = {};	// @combobox
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	cbAnScol.change = function cbAnScol_change (event)// @startlock
+	{// @endlock
+		var vAnScol = $$("component1_cbAnScol").getValue();
+		sources.component1_parcours_Scolaire.query("Annee_Scolaire.ID = :1 order by Eleve.Nom_complet", vAnScol);
+		
+	};// @lock
+
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_cbAnScol", "change", cbAnScol.change, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
